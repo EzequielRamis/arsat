60e1ee45b44e5115052fff74972e6e65e56fd0f7
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2021-01-05 17:20:54 -0300

    animated chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import { ParentSize } from "@visx/responsive";
 import { useState, useEffect } from "react";
-import { Price, Pair, TimeRange, step } from "./utils";
-import get from "axios";
+import { Price, /*Pair, TimeRange,*/ step } from "./utils";
+// import get from "axios";
 import { Chart, yScaleT, Theme } from "./Chart";
-import { appleStock } from "@visx/mock-data";
-import { parseISO } from "date-fns";
+import { genDateValue } from "@visx/mock-data";
+// import { parseISO } from "date-fns";
 
-async function getPrices(p: Pair, r: TimeRange): Promise<Price[]> {
-  return await get(`/api/prices/${p}?from=${r}`).then((res) => res.data);
+// async function getPrices(p: Pair, r: TimeRange): Promise<Price[]> {
+//   return await get(`/api/prices/${p}?from=${r}`).then((res) => res.data);
+// }
+
+function randomEnum<T>(anEnum: T): T[keyof T] {
+  const enumValues = (Object.values(anEnum) as unknown) as T[keyof T][];
+  const randomIndex = Math.floor(Math.random() * enumValues.length);
+  return enumValues[randomIndex];
 }
 
 function App() {
   const [data, setData] = useState<Price[]>([]);
+  const [theme, setTheme] = useState<Theme>(Theme.Blue);
+  const updateData = (n: number) => {
+    let prices = genDateValue(n).map((p) => {
+      let price: Price = {
+        date: p.date,
+        value: p.value,
+      };
+      return price;
+    });
+    setTheme(randomEnum(Theme));
+    setData(step(1, prices));
+  };
+
   useEffect(() => {
     // let [pair, ts] = [Pair.BTCUSD, TimeRange.Max];
     // getPrices(pair, ts)
@@ -26,28 +45,24 @@ function App() {
     //     setData(step(35, prices));
     //   })
     //   .catch((err) => console.error(err));
-    let prices = appleStock.map((p) => {
-      let price: Price = {
-        date: parseISO(p.date),
-        value: p.close,
-      };
-      return price;
-    });
-    console.log(step(2, prices));
-    setData(step(10, prices));
+    updateData(50);
   }, []);
+
   return (
-    <ParentSize>
-      {({ width, height }) => (
-        <Chart
-          width={width}
-          height={height}
-          data={data}
-          yScaleType={yScaleT.Linear}
-          theme={Theme.Blue}
-        />
-      )}
-    </ParentSize>
+    <>
+      <button onClick={() => updateData(50)}>Update chart</button>
+      <ParentSize>
+        {({ width, height }) => (
+          <Chart
+            width={width}
+            height={height}
+            data={data}
+            yScaleType={yScaleT.Log}
+            theme={theme}
+          />
+        )}
+      </ParentSize>
+    </>
   );
 }
 
--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Price } from "./utils";
 import { scaleLinear, scaleTime, scaleLog } from "@visx/scale";
-import { AreaClosed, Area } from "@visx/shape";
+import { AreaClosed } from "@visx/shape";
 import { curveNatural } from "@visx/curve";
 import { Group } from "@visx/group";
 import { LinearGradient } from "@visx/gradient";
 import { PatternLines } from "@visx/pattern";
+import { ClipPath } from "@visx/clip-path";
+import { Spring } from "react-spring/renderprops";
 import { extent } from "d3-array";
 
 export type PriceChartProps = {
@@ -25,6 +27,7 @@ export enum Theme {
   Orange = "#F7931A",
   Red = "#E0245E",
   Blue = "#00AAFF",
+  Green = "#00D588",
 }
 
 const lineWidth = 3;
@@ -55,57 +58,76 @@ export function Chart({
 
   const yScale = yScaleType === yScaleT.Linear ? yScaleLinear : yScaleLog;
 
+  const animateData = (props: number[]) => {
+    return data.map((p: Price, i: number) => ({
+      ...p,
+      value: props[i],
+    }));
+  };
+
   return (
     <svg width={width} height={height}>
+      <ClipPath id='clip-pattern'>
+        <rect x={0} y={0} width={width} height={height} />
+      </ClipPath>
+      <Spring to={{ theme }}>
+        {(props) => (
+          <>
+            <PatternLines
+              id='area-pattern'
+              height={20}
+              width={20}
+              stroke={`${theme}0A`}
+              strokeWidth={lineWidth}
+              orientation={["diagonal"]}
+              background='#241f3d'
+            />
+            <LinearGradient
+              id='line-gradient'
+              from={props.theme}
+              to={props.theme}
+              fromOpacity={0.2}
+              toOpacity={1}
+              vertical={false}
+            />
+            <LinearGradient
+              id='area-gradient'
+              from={props.theme}
+              to={props.theme}
+              fromOpacity={0.5}
+              toOpacity={0}
+            />
+          </>
+        )}
+      </Spring>
       <Group>
-        <PatternLines
-          id='area-pattern'
-          height={20}
-          width={20}
-          stroke={`${theme}0A`}
-          strokeWidth={lineWidth}
-          orientation={["diagonal"]}
-        />
-        <LinearGradient
-          id='line-gradient'
-          from={theme}
-          to={theme}
-          fromOpacity={0.5}
-          toOpacity={1}
-          vertical={false}
-        />
-        <LinearGradient
-          id='area-gradient'
-          from={theme}
-          to={theme}
-          fromOpacity={0.5}
-          toOpacity={0}
-        />
-        <AreaClosed
-          data={data}
-          yScale={yScale}
-          x={(p) => xScale(p.date)}
-          y={(p) => yScale(p.value)}
-          fill={"url(#area-pattern)"}
-          curve={curveNatural}
-        />
-        <Area
-          data={data}
-          x={(p) => xScale(p.date)}
-          y={(p) => yScale(p.value)}
-          stroke={"url(#line-gradient)"}
-          strokeWidth={lineWidth}
-          curve={curveNatural}
-        />
-        <AreaClosed
-          data={data}
-          yScale={yScale}
-          x={(p) => xScale(p.date)}
-          y={(p) => yScale(p.value)}
-          fill={"url(#area-gradient)"}
-          curve={curveNatural}
-        />
+        <Spring to={data.map(y)}>
+          {(props) => (
+            <>
+              <AreaClosed
+                id='chart'
+                data={animateData(props)}
+                yScale={yScale}
+                x={(p) => xScale(p.date)}
+                y={(p) => yScale(p.value)}
+                stroke={"url(#line-gradient)"}
+                strokeWidth={lineWidth}
+                curve={curveNatural}
+              />
+            </>
+          )}
+        </Spring>
       </Group>
+      <use
+        clipPath='url(#clip-pattern)'
+        xlinkHref='#chart'
+        fill='url(#area-pattern)'
+      />
+      <use
+        clipPath='url(#clip-pattern)'
+        xlinkHref='#chart'
+        fill={"url(#area-gradient)"}
+      />
     </svg>
   );
 }
