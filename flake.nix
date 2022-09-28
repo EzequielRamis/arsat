8f21295106e0b2012381a0442b857d3d98304a35
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2022-09-27 23:27:07 -0300

    node 12 deprecated

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs-16_x
+            yarn
+            nodePackages.typescript-language-server
+            nodePackages.prettier
+            nodePackages.vercel
+          ];
+        };
+      });
+}
